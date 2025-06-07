22a400e9c3a16ba4f33b41fcb428d62a5b75d532
João Souza <j.souza006@edu.pucrs.br>
2025-06-07 17:39:13 -0300

    Update consulta_produtos_maior_200.py

--- consultas/consulta_produtos_maior_200.py
+++ consultas/consulta_produtos_maior_200.py
@@ -7,3 +7,38 @@ res = bd.produtos.find(
 
 for doc in res:
     print(doc)
+
+
+
+# Consulta: Total de pedidos por cliente
+
+res = bd.pedidos.aggregate([
+    {
+        "$group": {
+            "_id": "$cod_cliente",
+            "total_pedidos": { "$sum": 1 }
+        }
+    },
+    {
+        "$lookup": {
+            "from": "clientes",
+            "localField": "_id",
+            "foreignField": "_id",
+            "as": "cliente"
+        }
+    },
+    { "$unwind": "$cliente" },
+    {
+        "$project": {
+            "cliente_id": "$_id",
+            "nome": "$cliente.nome",
+            "total_pedidos": 1,
+            "_id": 0
+        }
+    },
+    { "$sort": { "total_pedidos": -1 } }
+])
+
+for doc in res:
+    print(doc)
+
